0d04cf9456f6b8256604f2c5beb377076ef281dc
Mike Howles <entmike@gmail.com>
2015-08-14 10:53:36 -0500

    Clean up

--- bundle.js
+++
@@ -1,3 +0,0 @@
-var ncp = require('ncp').ncp;
-ncp('/lumira_sdk/lumira/bundles', '/Users/rhowles/.sapvi/extensions/bundles');
-ncp('/lumira_sdk/lumira/features', '/Users/rhowles/.sapvi/extensions/features');
